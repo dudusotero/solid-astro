a6888d9fd6b95844a6bac973f4cbeee55a57bbd1
Eduardo Sotero <eduardo.sotero@toptal.com>
2023-05-30 18:51:03 -0300

    Save changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import solidJs from '@astrojs/solid-js'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import solidJs from '@astrojs/solid-js';
+import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), solidJs(), sitemap()],
-})
+  output: "server",
+  adapter: vercel()
+});
